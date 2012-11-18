99a0e31639b88fd446a9f2033668f27fb6d00518
Vítek Ježek <vitek.jezek@clevis.cz>
2012-11-18 20:05:18 +0100

    [Homepage] zakladni layout JI (na zaklade prototypovani vedle od @HonzaSladek)

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -6,4 +6,17 @@
 abstract class BasePresenter extends Nette\Application\UI\Presenter
 {
 
+	/**
+	 * nastavi zakladni hodnoty do sablony
+	 *
+	 * @author Vitek Jezek
+	 **/
+	public function beforeRender()
+	{
+		$texy = new Texy();
+		$this->template->registerHelper('texy', callback($texy, 'process'));
+
+		$this->template->urlToCompetitionSystem = $this->context->parameters['urlToCompetitionSystem'];
+	}
+
 }
--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -1,14 +1,9 @@
 <?php
 
 /**
- * Homepage presenter.
+ * Zobrazeni webiku JI
  */
 class HomepagePresenter extends BasePresenter
 {
-
-	public function renderDefault()
-	{
-		$this->template->anyVariable = 'any value';
-	}
 
 }
