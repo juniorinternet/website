bb0fef59fdd460cfa01c954249071d052776d581
Vítek Ježek <vitek.jezek@clevis.cz>
2012-11-19 00:25:14 +0100

    [common] "error 404" se preposle na Homepage s vysvetlujici flashmessage

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -6,4 +6,36 @@
 class HomepagePresenter extends BasePresenter
 {
 
+	/**
+	 * zkontroluje, zda-li dany View existuje
+	 *
+	 * @author Vitek Jezek
+	 **/
+	public function beforeRender()
+	{
+		$templateFile = NULL;
+		$template = $this->getTemplate();
+		if ($template && $template instanceof Nette\Templating\IFileTemplate && !$template->getFile())
+		{
+			$files = $this->formatTemplateFiles();
+			foreach ($files as $file)
+			{
+				if (is_file($file))
+				{
+					$templateFile = $file;
+					break;
+				}
+			}
+
+			if (!$templateFile)
+			{
+				$file = preg_replace('#^.*([/\\\\].{1,70})$#U', "\xE2\x80\xA6\$1", reset($files));
+				$file = strtr($file, '/', DIRECTORY_SEPARATOR);
+				$this->flashMessage("V listopadu 2012 jsme sputili nový web. Stránka, kterou hledáte, jsme nejspíše přesunuli či úplně zrušili. Zkuste jí prosím najít ručně");
+				$this->redirect("default");
+			}
+		}
+
+		parent::beforeRender();
+	}
 }
