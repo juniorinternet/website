f76b0cb1a26e795382e0455b5e6f79e6991110e7
Jan Sládek <me@honzasladek.com>
2012-12-28 12:57:49 +0100

    Local changes

--- wp-config.php
+++ wp-config.php
@@ -22,7 +22,7 @@ define('DB_NAME', 'juniorinternet_blog');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
