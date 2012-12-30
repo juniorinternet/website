bf44b8498c3be26504c0310322ba3705f3203509
Martin Kucera <martin@mkucera.cz>
2012-12-30 17:16:17 +0100

    homepage

--- wp-content/themes/juniorinternet/index.php
+++ wp-content/themes/juniorinternet/index.php
@@ -1,3 +1,111 @@
 <?php get_header(); ?>
 
+<div role="banner" class="clearfix banner primary">
+	<div class="inner">
+		<span class="year">2013 &mdash; 14. roèník</span>
+		<h1>Junior Internet</h1>
+		<h2>Odrazový mùstek <span class="line-break"></span>pro mladé internetové nadšence.</h2>
+		<p>Již 14 let pomáháme mladým lidem poznávat internetové technologie, potkávat zajímavé lidi a prosadit se na internetu.</p>
+		<p class="nav quicknav" role="navigation">
+			Organizujeme:
+			<a n:href="competition">Soutìže</a>
+
+			<!-- <a href="/konference">Konference</a> -->
+			<span class="wip" title="V pøípravì">Konference</span>
+
+			<!-- <a href="/junior-camp">Junior&nbsp;Camp</a> -->
+			<span class="wip" title="V pøípravì">Junior&nbsp;Camp</span>
+		</p>
+
+		<?php //{include #mainNav} ?>
+	</div>
+
+</div>
+
+<div role="main" class="content clearfix secondary competition">
+
+	<div class="inner">
+		<h2>Soutìže</h2>
+		<p class="extraordinary-text">Ukaž co v tobì je. Pøihlaš projekt do soutìže a vyhraj hodnotné ceny.</p>
+		<p>Junior Internet se mùže stát tvým odrazovým mùstkem, který tì vyhoupne do svìta úspìšných mladých internetových webdesignerù, programátorù, internetových novináøù, èi náctiletých podnikatelù. {* Podívej se na <a n:href="successStories">pøíbìhy vítìzù</a> a jak jim Junior Internet zmìnil život.*}</p>
+		<p>Pøihlášky konèí <strong>17. února 2013 o pùlnoci</strong>.</p>
+		<div class="categories clearfix">
+			<div class="category">
+				<h3>Junior <span>Web</span></h3>
+				<div class="category-about">
+					<div>
+						<p>Webové stránky, internetové služby a projekty umístìné na Internetu.</p>
+					</div>
+				</div>
+			</div>
+			<div class="category">
+				<h3>Junior <span>Design</span></h3>
+				<div class="category-about">
+					<div>
+						<p>Design webových stránek èi jiné elektronické grafické práce.</p>
+					</div>
+				</div>
+			</div>
+			<div class="category">
+				<h3>Junior <span>Text</span></h3>
+				<div class="category-about">
+					<div>
+						<p>Slohová práce na téma "internet a vzdìlávání".</p>
+					</div>
+				</div>
+			</div>
+			<div class="category">
+				<h3>Junior <span>Erb</span></h3>
+				<div class="category-about">
+					<div>
+						<p>Obecnì prospìšné internetové projekty nebo stránky týkající se místa vašeho bydlištì.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<p>Letos udìlujeme také <strong>speciální ceny</strong> napøíè soutìžemi Junior Web a Junior Erb:</p>
+		<ul class="special-awards special-list">
+			<li>Technologické zpracování</li>
+			<li>Uživatelský prožitek na mobilech</li>
+			<li>Nadace Vodafone za obecnì prospìšný projekt</li>
+			<li>Cena veøejnosti</li>
+		</ul>
+		<p><a href="http://souteze.juniorinternet.cz/register/" class="btn btn-extlarge btn-primary">Pøihlásit se do soutìže</a></p>
+		<p>
+			<a n:href="competition">další informace</a> |
+			<a n:href="competition#pravidla">pravidla soutìže &amp; prùbìh hodnocení</a>
+		</p>
+	</div>
+
+</div><!-- / content -->
+
+<div role="complementary" class="content clearfix tertiary">
+
+	<div class="inner">
+		<p class="extraordinary-text">Pøipravujeme Junior Camp ve školách.</p>
+		<p>Jeden den plný nových webových technologií a týmové práce ve tvé škole.</p>
+	</div>
+
+</div><!-- / content -->
+
+<div role="complementary" class="content clearfix primary">
+
+	<div class="inner">
+		<h2>Konference</h2>
+		<div class="conference clearfix">
+			<div class="half left-side">
+				<p>Den plný nezapomenutelných pøednášek, nových kamarádù, zajímavých rozhovorù a finále soutìží Junior Internet.</p>
+				<p>Registrace na konferenci se otevøou v únoru 2013. Vstup je zdarma.</p>
+			</div>
+			<div class="half">
+				<div class="conference-date">
+					<i class="conference-day">23<span>.</span></i> <strong class="conference-year"><span>bøezen 2013</span></strong>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</div><!-- / content -->
+
+
 <?php get_footer(); ?>
