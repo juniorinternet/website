c478694f5d709217347cd5862ad0a26f164c2d8e
Martin Kucera <martin@mkucera.cz>
2012-12-31 13:40:01 +0100

    google analytics

--- wp-content/themes/juniorinternet/footer.php
+++ wp-content/themes/juniorinternet/footer.php
@@ -160,6 +160,19 @@ EOT;
 ML.feedback('http://juniorinternet.mailerlite.com/subscribe/join/s2l6n1/1');
 </script>
 
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36437500-1']);
+  _gaq.push(['_setDomainName', 'juniorinternet.cz']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+
 <?php wp_footer(); ?>
 
 </body>
