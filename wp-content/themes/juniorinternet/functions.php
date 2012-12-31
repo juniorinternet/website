904cce0018f12dd99cb2a9abe82945983282912b
Martin Kucera <martin@mkucera.cz>
2012-12-31 13:03:14 +0100

    store url for the competitions

--- wp-content/themes/juniorinternet/functions.php
+++ wp-content/themes/juniorinternet/functions.php
@@ -3,3 +3,5 @@
 add_filter('stylesheet_uri', function() {
     return get_stylesheet_directory_uri(). '/assets/css/screen.css';
 }); 
+
+add_option('urlToCompetitionSystem', 'http://souteze.juniorinternet.cz/register/');
