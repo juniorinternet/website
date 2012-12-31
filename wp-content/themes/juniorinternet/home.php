0654e09f620217183d1a2061737c0668b5e836cb
Martin Kucera <martin@mkucera.cz>
2012-12-31 13:22:59 +0100

    non-nette link

--- wp-content/themes/juniorinternet/home.php
+++ wp-content/themes/juniorinternet/home.php
@@ -3,7 +3,7 @@
 	<div class="inner">
 		<span class="year">2013 &mdash; 14. ročník</span>
 		
-		<p class="main-text">Co se <strong>aktuálně</strong> děje <span class="line-break-mobile"></span> v projektu <a n:href="default"><span>Junior Internet</span></a></p>
+		<p class="main-text">Co se <strong>aktuálně</strong> děje <span class="line-break-mobile"></span> v projektu <a href="<?php echo home_url() ?>"><span>Junior Internet</span></a></p>
 
 		<?php get_template_part( 'navigation' ); ?>
 	</div>
