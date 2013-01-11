591afdb5f330be28505aa8eac00360652ff69ae4
Martin Kucera <martin@mkucera.cz>
2013-01-11 22:29:59 +0100

    permalink

--- wp-content/themes/juniorinternet/home.php
+++ wp-content/themes/juniorinternet/home.php
@@ -16,7 +16,7 @@
 		<?php if ( have_posts() ): while ( have_posts() ) : the_post(); ?>
 		<div id="post-<?php the_ID(); ?>" <?php post_class('article'); ?>>
 			<span class="date"><?php the_date() ?></span>
-			<h3><?php the_title(); ?></h3>
+			<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 			<?php the_content(); ?>
 		</div>
 		<?php endwhile;  ?>
