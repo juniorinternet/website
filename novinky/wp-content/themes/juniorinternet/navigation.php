c6e38f2514b44b03ac6677688184b983a3ec5c37
Jan Sládek <me@honzasladek.com>
2013-02-03 15:44:31 +0100

    Aktualizace menu na Aktuálně

--- novinky/wp-content/themes/juniorinternet/navigation.php
+++ novinky/wp-content/themes/juniorinternet/navigation.php
@@ -4,10 +4,8 @@
 	<span class="wip" title="V přípravě">Předchozí&nbsp;ročníky</span>
 	<!-- <a href="/pribehy-vitezu">Příběhy&nbsp;vítězů</a> -->
 	<span class="wip" title="V přípravě">Příběhy&nbsp;vítězů</span>
-	<!-- <a href="/o-projektu">O&nbsp;projektu</a> -->
-	<span class="wip" title="V přípravě">O&nbsp;projektu</span>
-	<!-- <a href="/pro-media">Pro&nbsp;média</a> -->
-	<span class="wip" title="V přípravě">Pro&nbsp;média</span>
+	<a href="/o-projektu">O&nbsp;projektu</a>
+	<a href="/pro-media">Pro&nbsp;média</a>
 	<a href="/kontakt">Kontakt</a>
 </div>
 
