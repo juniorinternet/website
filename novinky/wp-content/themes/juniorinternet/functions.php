1e30fb0848a3c78324e7e925770198ab2765c2f7
Martin Kucera <martin@mkucera.cz>
2013-02-23 22:49:35 +0100

    function for creating links to nette pages

--- novinky/wp-content/themes/juniorinternet/functions.php
+++ novinky/wp-content/themes/juniorinternet/functions.php
@@ -5,3 +5,19 @@ add_filter('stylesheet_uri', function() {
 }); 
 
 add_option('urlToCompetitionSystem', 'http://souteze.juniorinternet.cz/register/');
+
+
+function netteLink($destination)
+{  
+  require_once ABSPATH. '../libs/autoload.php';
+
+  $a = strrpos($destination, ':');
+  $presenter = substr($destination, 0, $a);
+  $action = $a === strlen($destination) - 1 ? NULL : substr($destination, $a + 1);
+  
+  $request = new Nette\Application\Request( $presenter, 'GET', array('action' => $action) );
+  
+  $router = require ABSPATH. '../app/routes.php';
+
+  return $router->constructUrl( $request, new Nette\Http\Url( home_url("/") ) );
+}
