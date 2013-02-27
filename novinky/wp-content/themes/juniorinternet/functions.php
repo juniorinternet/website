eea55e34e49aaa521b6e5c02534a646972e8bf46
Martin Kucera <martin@mkucera.cz>
2013-02-27 17:45:57 +0100

    the router variable is static and the routes won't be included more times

--- novinky/wp-content/themes/juniorinternet/functions.php
+++ novinky/wp-content/themes/juniorinternet/functions.php
@@ -10,14 +10,16 @@ add_option('urlToCompetitionSystem', 'http://souteze.juniorinternet.cz/register/
 function netteLink($destination)
 {  
   require_once ABSPATH. '../libs/autoload.php';
+  
+  static $router = NULL;
+  
+  if ( !isset($router) ) $router = require ABSPATH. '../app/routes.php';
 
   $a = strrpos($destination, ':');
   $presenter = substr($destination, 0, $a);
   $action = $a === strlen($destination) - 1 ? NULL : substr($destination, $a + 1);
   
   $request = new Nette\Application\Request( $presenter, 'GET', array('action' => $action) );
-  
-  $router = require ABSPATH. '../app/routes.php';
 
   return $router->constructUrl( $request, new Nette\Http\Url( home_url("/") ) );
 }
