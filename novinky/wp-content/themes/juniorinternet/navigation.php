c5245a15e47fd259890d15a3927a61d2d546b36d
Martin Kucera <martin@mkucera.cz>
2013-02-27 22:28:52 +0100

    this is not being used anymore

--- novinky/wp-content/themes/juniorinternet/navigation.php
+++
@@ -1,9 +0,0 @@
-<div class="nav mainnav" role="navigation">
-	<a href="<?php echo netteLink('Homepage:news') ?>">Aktuálně</a>
-	<a href="<?php echo netteLink('Homepage:previous') ?>">Předchozí&nbsp;ročníky</a>
-	<a href="<?php echo netteLink('Homepage:successStories') ?>">Příběhy&nbsp;vítězů</a>
-	<a href="<?php echo netteLink('Homepage:about') ?>">O&nbsp;projektu</a>
-	<a href="<?php echo netteLink('Homepage:media') ?>">Pro&nbsp;média</a>
-	<a href="<?php echo netteLink('Homepage:contact') ?>">Kontakt</a>
-</div>
-
