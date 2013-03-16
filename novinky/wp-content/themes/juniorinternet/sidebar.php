88baffb11c11fa506a47f4db936595f5d031e8b6
Vítek Ježek <vitek.jezek@clevis.cz>
2013-03-16 14:27:39 +0100

    Sidebar: zakomentování soutěží, promotování konference

--- novinky/wp-content/themes/juniorinternet/sidebar.php
+++ novinky/wp-content/themes/juniorinternet/sidebar.php
@@ -2,19 +2,11 @@
 	<h3>O Junior Internetu</h3>
 	<p>Již 14 let pomáháme mladým lidem poznávat internetové technologie, potkávat zajímavé lidi a prosadit se na internetu.</p>
 	<h3>Pořádáme</h3>
-	<div class="organise">
-		<h4>Soutěže</h4>
-		<p>Ukaž co v tobě je. Přihlaš projekt do soutěže a vyhraj hodnotné ceny.</p>
-		<p>
-			<a class="btn btn-primary" href="<?php echo get_option('urlToCompetitionSystem') ?>">přihlásit projekt</a>
-			<span class="line-break-tablet"></span>
-			<a href="http://www.juniorinternet.cz/souteze">více informací</a>
-		</p>
-	</div>
+
 	<div class="organise">
 		<h4>Konference</h4>
 		<p>23. března 2013 zažijete den plný nezapomenutelných přednášek, nových kamarádů, zajímavých rozhovorů a finále soutěží Junior Internet.</p>
-		<p>Registrace na konferenci se otevřou v únoru 2013.</p>
+		<p>Konference je zdarma, je však třeba se <a class="btn btn-primary" href="http://www.juniorinternet.cz/konference">zaregistrovat</a></p>
 	</div>
 
 </div>
