cbd87e5e04ab7ce7e1c71bd1c51cc236b15f1327
Jan Sládek <me@honzasladek.com>
2013-04-09 00:56:19 +0200

    Aktualne - aside Pripravujeme

--- novinky/wp-content/themes/juniorinternet/sidebar.php
+++ novinky/wp-content/themes/juniorinternet/sidebar.php
@@ -1,12 +1,12 @@
 <div role="navigation" class="aside">
 	<h3>O Junior Internetu</h3>
 	<p>Již 14 let pomáháme mladým lidem poznávat internetové technologie, potkávat zajímavé lidi a prosadit se na internetu.</p>
-	<h3>Pořádáme</h3>
+	<h3>Připravujeme</h3>
 
 	<div class="organise">
-		<h4>Konference</h4>
-		<p>23. března 2013 zažijete den plný nezapomenutelných přednášek, nových kamarádů, zajímavých rozhovorů a finále soutěží Junior Internet.</p>
-		<p>Konference je zdarma, je však třeba se <a class="btn btn-primary" href="http://www.juniorinternet.cz/konference">zaregistrovat</a></p>
+		<h4>Junior Camp</h4>
+		<p>Třídenní příměstský tábor, kde se mladí lidé naučí spolupracovat v týmu a vytvoří vlastní webovou aplikaci.</p>
+		<p>Přihlášky již brzy.</p>
 	</div>
 
 </div>
